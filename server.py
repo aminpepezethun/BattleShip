c6cc171d4c0120924bf081936b819e270e5120b7
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2025-04-28 14:07:53 +0800

    Adjusting some main function logic in server.py

--- server.py
+++ server.py
@@ -33,41 +33,40 @@ def send(wfile, msg):
     wfile.write(msg + "\n")
     wfile.flush()
 
-def handle_client(conn, addr):
+def handle_client(conn):
     client_id = str(uuid.uuid4())
     while conn:
         rfile = conn.makefile('r')
         wfile = conn.makefile('w')
         send(wfile, f"[INFO] Client {client_id} successfully connected to server. Waiting for other players to join")
         with LOCK:
-            CLIENT_QUEUE.append((rfile, wfile, client_id, addr))
+            CLIENT_QUEUE.append((rfile, wfile, client_id))
 
-def match_making():
-    with LOCK:
-        if len(CLIENT_QUEUE) >= 2:
-            p1 = CLIENT_QUEUE.popleft()
-            p2 = CLIENT_QUEUE.popleft()
-
-            threading.Thread(target=run_two_player_game_online, args=(p1[0], p1[1], p2[0], p2[1]), daemon=True).start()
-
-            print(f"[GAME] Paired {p1[3]} and {p2[3]}")
-    time.sleep(1)
+# def match_making():
+#     with LOCK:
+#         if len(CLIENT_QUEUE) >= 2:
+#             p1 = CLIENT_QUEUE.popleft()
+#             p2 = CLIENT_QUEUE.popleft()
+#
+#             threading.Thread(target=run_two_player_game_online, args=(p1[0], p1[1], p2[0], p2[1]), daemon=True).start()
+#
+#             print(f"[GAME] Paired {p1[3]} and {p2[3]}")
+#     time.sleep(1)
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen()
-
+        s.listen(10)
         print(f"[INFO] Server listening on {HOST}:{PORT}")
-        threading.Thread(target=match_making).start()
+        conn, addr = s.accept()
 
         while True:
-            conn, addr = s.accept()
             print(f"[INFO] New client connected from {addr}")
-            threading.Thread(
+            client_thread = threading.Thread(
                 target=handle_client,
-                args=(conn, addr)
-            ).start()
+                args=(conn,)
+            )
+            client_thread.start()
 
 
 # HINT: For multiple clients, you'd need to:
